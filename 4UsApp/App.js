30431e0b906972eef05ae2d8ca70546d4f01b1de
Daniel Reyes <daniel_reyes@javeriana.edu.co>
2022-05-04 13:05:34 -0500

    Implemented mySql backEnd With ExpressJs

--- 4UsApp/App.js
+++ 4UsApp/App.js
@@ -150,5 +150,156 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
+export default function App() {
+  console.log("App")
+  console.log("App")
+  console.log("App")
+  console.log("App")
+  return (
+    <View style={styles.container}>
+      <Text>Hola Daniel</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
